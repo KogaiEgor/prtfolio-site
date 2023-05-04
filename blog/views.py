66171c171b0cc280162e6b28bef4d5db66fc62e6
Egor <kogayegor@gmail.com>
2023-05-04 12:44:42 +0300

    Переделал все на drf

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Blog
+from .serializers import BlogSerializer
+from rest_framework import views
+from rest_framework.permissions import AllowAny
 
-def all_blogs(request):
 
-    projects_blog = Blog.objects.order_by('-id')[0:5]
-    return render(request, 'blog/all_blogs.html', {'blogs': projects_blog})
+class BlogsView(views.APIView):
+    permission_classes = [AllowAny]
+    serializer_class = BlogSerializer
 
-def detail(request, blog_id):
-    blog = get_object_or_404(Blog, pk=blog_id)
-    return render(request, 'blog/detail.html', {'blog':blog})
+    def get(self, request, *args, **kwargs):
+        queryset = Blog.objects.order_by('-id')[0:5]
+        return render(request, 'blog/all_blogs.html', {'blogs': queryset})
+
+class DetailView(views.APIView):
+    permission_classes = [AllowAny]
+    serializer_class = BlogSerializer
+
+    def get(self, request, blog_id, *args, **kwargs):
+        blog = get_object_or_404(Blog, pk=blog_id)
+        return render(request, 'blog/detail.html', {'blog': blog})
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from .models import Project
+from .serializer import ProjectsSerializer
+from rest_framework import views
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-def home(request):
-    projects = Project.objects.all()
-    return render(request, 'portfolio/home.html', {'projects': projects})
+
+class ProjectsView(views.APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = ProjectsSerializer
+
+    def get(self, request, *args, **kwargs):
+        queryset = Project.objects.all()
+        return render(request, 'portfolio/home.html', {'projects': queryset})
+
+
